d7b62cb08b41f03f3b9698fbc7ae0974b8f97b35
Alex Somarriba <alex@teambond.io>
2022-08-01 20:16:44 -0500

    added CheckStats Function

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-let arrayOfPosts = [];
 
-function fetchPokemonData(pokemon) {
+
+function FetchPokemonData(pokemon) {
+    let data;
     fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
     .then(res => {
       if(!res.ok) {
         throw Error(res.statusText)
       } return res.json()
     })
-    .then(posts => arrayOfPosts = posts)
-    .catch(err => console.log(`Error,  ${err}`))
+    .then(response => data = response.json);
+
+    return data
   }
 
+function CheckStats(pokemon1, pokemon2) {
+    let pokeData1 = FetchPokemonData(pokemon1)["stats"];
+    let pokeData2 = FetchPokemonData(pokemon2)["stats"];
+
+    let statTotal1 = 0;
+    pokeData1.forEach(stat => {
+        statTotal1 = statTotal1 + stat["base_stat"];
+    });
+
+    let statTotal2 = 0;
+    pokeData1.forEach(stat => {
+        statTotal2 = statTotal2 + stat["base_stat"];
+    });
+
+    if(statTotal1 > statTotal2) {
+        return pokemon1
+    } else if(statTotal2 > statTotal1) {
+        return pokemon2
+    }
+}
